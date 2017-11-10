6c51631a6b9a770e42fdcf7d844bc5ada998310c
Josep Dols <jodoldar@gmail.com>
2017-11-10 20:41:44 +0100

    Added VS things

---
+++ VS/LocalServer/LocalServer/ClaseMain.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocalServer
+{
+    public class ClaseMain
+    {
+        public static void StartListening()
+        {
+            // DATA INICIALIZATION
+            byte[] bytes;   // Data buffer for incoming data.
+            IPHostEntry ipHostInfo;
+            IPAddress ipAddress;
+        }
+        public static int Main(string[] args)
+        {
+            //Your program starts here...
+            // https://msdn.microsoft.com/es-es/library/6y0e13d3(v=vs.110).aspx
+            StartListening();
+            return 0;
+        }
+    }
+}
